68bbe881f2607d8f4bf0348251110697f50042ea
beeerlian <naufal.berlian.99@gmail.com>
2022-10-19 20:15:21 +0700

    update: password validation error message

--- src/middlewares/validator/users.validator.js
+++ src/middlewares/validator/users.validator.js
@@ -2,10 +2,10 @@ const { body, query } = require("express-validator");
 
 exports.basicCreds = [
 	body("email").isEmail().normalizeEmail().withMessage("Email is invalid"),
-	body("password").isLength({ min: 8 }).isStrongPassword({ minNumbers: 0 }).withMessage("Password must be 8 character or more"),
+	body("password").isLength({ min: 8 }).isStrongPassword({ minNumbers: 0 }).withMessage("Password must be 8 character or more and at least contain 1 symbol, 1 uppercase, and 1 lowercase"),
 ];
 exports.passwordOnlyCreds = [
-	body("password").isLength({ min: 8 }).isStrongPassword({ minNumbers: 0 }).withMessage("Password must be 8 character or more"),
+	body("password").isLength({ min: 8 }).isStrongPassword({ minNumbers: 0 }).withMessage("Password must be 8 character or more and at least contain 1 symbol, 1 uppercase, and 1 lowercase"),
 ];
 exports.emailOnlyCreds = [
 	body("email").isEmail().normalizeEmail().withMessage("Email is invalid"),
