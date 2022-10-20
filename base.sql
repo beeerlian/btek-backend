dba9d62e9a95be652388ee0b8d5e42a7b4be5683
beeerlian <naufal.berlian.99@gmail.com>
2022-10-20 19:54:24 +0700

    update: base.sql

--- base.sql
+++ base.sql
@@ -1,15 +1,13 @@
--- Active: 1665858920600@@127.0.0.1@5432@btek-course@public
+-- Active: 1665926832520@@127.0.0.1@5432@postgres@public
 
 CREATE EXTENSION 'uuid-ossp';
 
 CREATE OR REPLACE FUNCTION TRIGGER_SET_TIMESTAMP() 
-RETURNS TRIGGER AS 
-	$$ BEGIN NEW."updatedAt" = NOW();
+RETURNS TRIGGER AS $$ 
+	BEGIN NEW."updatedAt" = NOW();
 	RETURN NEW;
-	END 
-$ 
-
-$ LANGUAGE PLPGSQL;
+	END $ $ LANGUAGE 
+PLPGSQL; 
 
 CREATE TABLE
     "users" (
@@ -22,27 +20,64 @@ CREATE TABLE
 
 alter table "users" add primary key ("id");
 
+DROP TABLE "profile";
+
 CREATE TABLE
     "profile"(
         "id" VARCHAR(255) default uuid_generate_v4(),
+        "userId" VARCHAR(255),
         "fullName" VARCHAR(255),
         "picture" VARCHAR(255),
         "birthDate" VARCHAR(255),
         "createdAt" TIMESTAMPTZ default now(),
         "updatedAt" TIMESTAMPTZ
-    )
-alter table "profile"
-add primary key ("id");
+    );
+
+alter table "profile" add primary key ("id");
+
+CREATE TABLE
+    "forgot_passwords"(
+        "id" VARCHAR(255) default uuid_generate_v4(),
+        "code" VARCHAR(255) default uuid_generate_v4(),
+        "userId" VARCHAR(255),
+        "email" VARCHAR(255),
+        "available" BOOLEAN DEFAULT true,
+        "createdAt" TIMESTAMPTZ default now(),
+        "updatedAt" TIMESTAMPTZ
+    );
+
+alter table "forgot_passwords" add primary key ("id");
+
+DROP TABLE "forgot_passwords";
+
+CREATE TRIGGER SET_TIMESTAMP 
+	before
+	update
+	    on "forgot_passwords" for each row
+	execute
+	    procedure trigger_set_timestamp();
+; 
 
 CREATE TRIGGER SET_TIMESTAMP 
 	before
 	update on "users" for each row
 	execute
 	    procedure trigger_set_timestamp();
+; 
+
+;
+
+;
 
 CREATE TRIGGER SET_TIMESTAMP 
-		before
-		update on "profile" for each row
-		execute
-		    procedure trigger_set_timestamp();
-	
+	before
+	update on "profile" for each row
+	execute
+	    procedure trigger_set_timestamp();
+; 
+
+;
+
+;
+
+SELECT id,email,password,"createdAt","updatedAt" FROM users;
