a6ce13dba98576a0f51697cbe1a000dc19105f66
beeerlian <naufal.berlian.99@gmail.com>
2022-10-23 17:21:08 +0700

    refractor: folder structure

--- frontend/src/apis/client.api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const get = (url) => {
-  return axios.get(url);
-}
-
-export default { get }
--- frontend/src/repositories/character.repository.js
+++
@@ -1,7 +0,0 @@
-import client from '../apis/client.api'
-
-const getAllCharacter = () => {
-  return client.get('https://rickandmortyapi.com/api/character');
-}
-
-export default { getAllCharacter }
