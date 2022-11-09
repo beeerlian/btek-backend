55061461c8a7d7f9bd8e9ddd858d432bd047ab90
beeerlian <naufal.berlian.99@gmail.com>
2022-11-09 17:32:43 +0700

    remove: hardcode secret

--- src/middlewares/upload/cloudinary.upload.js
+++ src/middlewares/upload/cloudinary.upload.js
@@ -1,6 +1,7 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const { randomString } = require("../../utils");
+require("dotenv").config;
 
 const extGenerator = (mimetype) => {
 	const mime = ["image/jpeg", "image/png", "image/webp"];
@@ -9,9 +10,10 @@ const extGenerator = (mimetype) => {
 };
 
 cloudinary.config({
-	cloud_name: "dngflskzs",
-	api_key: "967883343469114",
-	api_secret: "xaMDGJAuQjHSWWVjVNFAa5WQTRU"
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET
+
 });
 
 const storage = new CloudinaryStorage({
